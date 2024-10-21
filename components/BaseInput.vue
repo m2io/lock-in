2e0b29b53d6823f1d95bfbc924b98d6307509f0c
m2io <grmnn@pm.me>
2024-10-21 17:06:01 +0200

    feat: implement input component

---
+++ components/BaseInput.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps<{
+	id?: string
+	label?: string
+	placeholder: string
+}>()
+
+const modelValue = defineModel<string>()
+</script>
+
+<template>
+	<label
+		v-if="label"
+		:for="id"
+		class="flex flex-col gap-1"
+	>
+		{{ label }}
+	</label>
+	<input
+		:id="id"
+		v-model="modelValue"
+		class="border border-zinc-700 p-2 bg-zinc-800 rounded w-full"
+		:placeholder="placeholder"
+	/>
+</template>
