5a6b0f8a9e2e71f19b06e041cadbaf34efb431bc
m2io <grmnn@pm.me>
2024-10-21 22:34:17 +0200

    feat: implement disable behaviour and adapt styles

--- components/BaseButton.vue
+++ components/BaseButton.vue
@@ -1,18 +1,20 @@
 <script setup lang="ts">
-const { type = 'button', variant = 'primary' } = defineProps<{
+const { type = 'button', variant = 'primary', disabled = false } = defineProps<{
 	type?: 'button' | 'submit'
 	variant?: 'primary' | 'delete'
+	disabled?: boolean
 }>()
 </script>
 
 <template>
 	<button
-		class="p-2 bg-zinc-800 rounded transition-colors duration-100"
+		class="rounded transition-colors duration-100 disabled:opacity-50 disabled:bg-zinc-500 disabled:text-zinc-800"
 		:class="{
-			'bg-indigo-500 hover:bg-indigo-400': variant === 'primary',
-			'bg-red-500 hover:bg-red-400': variant === 'delete',
+			'bg-indigo-500 hover:bg-indigo-400 p-2': variant === 'primary',
+			'p-1 text-zinc-500 hover:text-zinc-200': variant === 'delete',
 		}"
 		:type="type"
+		:disabled="disabled"
 	>
 		<slot></slot>
 	</button>
