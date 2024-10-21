4d8b2cc9a56eae25d710640be45b8dddab6cc36b
m2io <grmnn@pm.me>
2024-10-21 17:05:34 +0200

    feat: implement button component

---
+++ components/BaseButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const { type = 'button', variant = 'primary' } = defineProps<{
+	type?: 'button' | 'submit'
+	variant?: 'primary' | 'delete'
+}>()
+</script>
+
+<template>
+	<button
+		class="p-2 bg-zinc-800 rounded transition-colors duration-100"
+		:class="{
+			'bg-indigo-500 hover:bg-indigo-400': variant === 'primary',
+			'bg-red-500 hover:bg-red-400': variant === 'delete',
+		}"
+		:type="type"
+	>
+		<slot></slot>
+	</button>
+</template>
