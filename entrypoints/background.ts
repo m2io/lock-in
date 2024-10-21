df6ab67617fc1059c11b52d08aa9c203ce46198c
m2io <grmnn@pm.me>
2024-10-21 23:36:22 +0200

    feat: navigate to new url when watched website matches

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,3 +1,30 @@
-export default defineBackground(() => {
-	console.log('Hello background!', { id: browser.runtime.id })
+import { browser } from 'wxt/browser'
+
+export default defineBackground({
+	// Set manifest options
+	persistent: true,
+	type: 'module',
+
+	main() {
+		const { state, getWebsiteEntries } = useWebsiteController()
+
+		browser.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+			if (changeInfo.status === 'complete' && tab.url) {
+				await getWebsiteEntries()
+				const currentUrl = new URL(tab.url!).hostname
+
+				const matchedWebsite = state.value.websites.find((site) => currentUrl.includes(site.url))
+				if (!matchedWebsite) {
+					return
+				}
+
+				if (matchedWebsite.blocked) {
+					browser.tabs.update(tabId, { url: browser.runtime.getURL('/lock-in.html') })
+				} else {
+					// Implement time tracking logic here
+					console.log(`Tracking time spent on ${currentUrl}`)
+				}
+			}
+		})
+	},
 })
