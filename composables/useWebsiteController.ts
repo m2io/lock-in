ce31add63801c00b57a6b9c4d0a17362941ca618
m2io <grmnn@pm.me>
2024-10-21 22:35:03 +0200

    feat: implement CRUD operations for website list

---
+++ composables/useWebsiteController.ts
@@ -0,0 +1,56 @@
+import { storage } from 'wxt/storage'
+
+interface StorageState {
+	websites: Website[]
+}
+
+export interface Website {
+	url: string
+	blocked: boolean
+}
+
+const state = ref<StorageState>({
+	websites: [],
+})
+
+const initialWebsitesState = ref<Website[]>([])
+
+export function useWebsiteController() {
+	async function getWebsiteEntries() {
+		const storedWebsites = await storage.getItem<string>('local:websites')
+
+		if (storedWebsites) {
+			state.value.websites = JSON.parse(storedWebsites)
+			initialWebsitesState.value = JSON.parse(storedWebsites)
+		}
+	}
+
+	async function updateWebsiteEntries() {
+		const currentStateStringified = JSON.stringify(state.value.websites)
+		await storage.setItem('local:websites', currentStateStringified)
+		initialWebsitesState.value = JSON.parse(currentStateStringified)
+	}
+
+	async function createWebsiteEntry(website: Website) {
+		state.value.websites.push(website)
+		const currentStateStringified = JSON.stringify(state.value.websites)
+		await storage.setItem('local:websites', currentStateStringified)
+		initialWebsitesState.value = JSON.parse(currentStateStringified)
+	}
+
+	async function deleteWebsiteEntry(index: number) {
+		state.value.websites.splice(index, 1)
+		const currentStateStringified = JSON.stringify(state.value.websites)
+		await storage.setItem('local:websites', currentStateStringified)
+		initialWebsitesState.value = JSON.parse(currentStateStringified)
+	}
+
+	return {
+		state,
+		initialWebsitesState,
+		createWebsiteEntry,
+		getWebsiteEntries,
+		updateWebsiteEntries,
+		deleteWebsiteEntry,
+	}
+}
--- entrypoints/popup/App.vue
+++ entrypoints/popup/App.vue
@@ -1,44 +1,107 @@
-<script lang="ts" setup>
-import HelloWorld from '@/components/HelloWorld.vue'
+<script setup lang="ts">
+import BaseButton from '@/components/BaseButton.vue'
+import BaseCheckbox from '@/components/BaseCheckbox.vue'
+import BaseInput from '@/components/BaseInput.vue'
+
+import { useWebsiteController } from '@/composables/useWebsiteController'
+import { onMounted, ref } from 'vue'
+
+const { state, initialWebsitesState, createWebsiteEntry, getWebsiteEntries, updateWebsiteEntries, deleteWebsiteEntry } = useWebsiteController()
+
+onMounted(async () => {
+	await getWebsiteEntries()
+})
+
+const newWebsite = ref('')
+async function handleSubmit() {
+	await createWebsiteEntry({ url: newWebsite.value, blocked: true })
+	newWebsite.value = ''
+}
+
+const updateButtonDisabled = ref(true)
+watch(
+	() => state.value.websites,
+	(nv) => {
+		if (nv.length !== 0 && nv.length === initialWebsitesState.value.length) {
+			const currentStateChanged = nv.some((site, index) => {
+				if (site.blocked !== initialWebsitesState.value[index].blocked || site.url !== initialWebsitesState.value[index].url) {
+					return true
+				}
+
+				return false
+			})
+
+			updateButtonDisabled.value = !currentStateChanged
+		}
+	},
+	{
+		deep: true,
+	},
+)
+
+async function handleUpdate() {
+	await updateWebsiteEntries()
+	updateButtonDisabled.value = true
+}
 </script>
 
 <template>
-	<div>
-		<a
-			href="https://wxt.dev"
-			target="_blank"
+	<div class="p-4 flex flex-col gap-y-4">
+		<form
+			class="flex gap-x-2"
+			@submit.prevent="handleSubmit"
 		>
-			<img
-				src="/wxt.svg"
-				class="logo"
-				alt="WXT logo"
+			<BaseInput
+				id="new-website"
+				v-model="newWebsite"
+				placeholder="youtube.com/shorts, x.com"
 			/>
-		</a>
-		<a
-			href="https://vuejs.org/"
-			target="_blank"
+			<BaseButton
+				type="submit"
+				class="whitespace-nowrap"
+			>
+				Block Site
+			</BaseButton>
+		</form>
+		<ul
+			v-if="state.websites.length"
+			class="flex flex-col gap-y-2"
 		>
-			<img
-				src="@/assets/vue.svg"
-				class="logo vue"
-				alt="Vue logo"
-			/>
-		</a>
-	</div>
-	<HelloWorld msg="WXT + Vue" />
-</template>
+			<li
+				v-for="(site, index) in state.websites"
+				:key="index"
+				class="flex items-center gap-x-4"
+			>
+				<BaseCheckbox
+					:id="`checkbox-${index}`"
+					v-model="site.blocked"
+				/>
+				<BaseInput
+					v-model="site.url"
+					placeholder="youtube.com/shorts or x.com"
+				/>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #54bc4ae0);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+				<BaseButton
+					variant="delete"
+					@click="deleteWebsiteEntry(index)"
+				>
+					<svg
+						class="w-5 h-5"
+						viewBox="0 0 24 24"
+					><path
+						fill="currentColor"
+						d="M7 21q-.825 0-1.412-.587T5 19V6q-.425 0-.712-.288T4 5t.288-.712T5 4h4q0-.425.288-.712T10 3h4q.425 0 .713.288T15 4h4q.425 0 .713.288T20 5t-.288.713T19 6v13q0 .825-.587 1.413T17 21zM17 6H7v13h10zM7 6v13zm5 7.9l1.9 1.9q.275.275.7.275t.7-.275t.275-.7t-.275-.7l-1.9-1.9l1.9-1.9q.275-.275.275-.7t-.275-.7t-.7-.275t-.7.275L12 11.1l-1.9-1.9q-.275-.275-.7-.275t-.7.275t-.275.7t.275.7l1.9 1.9l-1.9 1.9q-.275.275-.275.7t.275.7t.7.275t.7-.275z"
+					/></svg>
+				</BaseButton>
+			</li>
+		</ul>
+		<BaseButton
+			v-if="state.websites.length"
+			class="w-full"
+			:disabled="updateButtonDisabled"
+			@click="handleUpdate"
+		>
+			Sync changes
+		</BaseButton>
+	</div>
+</template>
