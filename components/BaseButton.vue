39637915f559617653978fe2d83bd9ff6e08c796
m2io <grmnn@pm.me>
2024-10-23 21:34:49 +0200

    fix: go pack to single app.vue without vue-router
    * implement icon usage directly via BaseButton.vue component

--- components/BaseButton.vue
+++ components/BaseButton.vue
@@ -1,21 +1,32 @@
 <script setup lang="ts">
-const { type = 'button', variant = 'primary', disabled = false } = defineProps<{
+import BaseIcon from '@/components/BaseIcon.vue'
+
+const { type = 'button', variant = 'primary', icon = null, disabled = false } = defineProps<{
 	type?: 'button' | 'submit'
-	variant?: 'primary' | 'delete'
+	variant?: 'primary' | 'ghost'
+	icon?: keyof typeof _icons
+	iconSize?: string
 	disabled?: boolean
 }>()
+
+const _icons = useIcons()
 </script>
 
 <template>
 	<button
 		class="rounded transition-colors duration-100 disabled:opacity-50 disabled:bg-zinc-500 disabled:text-zinc-800"
 		:class="{
-			'bg-indigo-500 hover:bg-indigo-400 p-2': variant === 'primary',
-			'p-1 text-zinc-500 hover:text-zinc-200': variant === 'delete',
+			'bg-indigo-500 hover:bg-indigo-400 p-2': variant === 'primary' && !icon,
+			'p-1 text-zinc-500 hover:text-zinc-200': variant === 'ghost' || icon,
 		}"
 		:type="type"
 		:disabled="disabled"
 	>
 		<slot></slot>
+		<BaseIcon
+			v-if="icon"
+			:name="icon"
+			:size="iconSize"
+		/>
 	</button>
 </template>
--- entrypoints/popup/App.vue
+++ entrypoints/popup/App.vue
@@ -1,21 +1,102 @@
 <script setup lang="ts">
 import AppIconsSprite from '@/components/AppIconsSprite.vue'
-import { useWebsiteController } from '@/composables/useWebsiteController'
-import { onMounted } from 'vue'
-import { RouterView } from 'vue-router'
+import BaseButton from '@/components/BaseButton.vue'
+import BaseCheckbox from '@/components/BaseCheckbox.vue'
+import BaseInput from '@/components/BaseInput.vue'
 
-const { getWebsiteEntries } = useWebsiteController()
+import { useWebsiteController } from '@/composables/useWebsiteController'
+import { onMounted, ref, watch } from 'vue'
+
+const { state, initialWebsitesState, createWebsiteEntry, getWebsiteEntries, updateWebsiteEntries, deleteWebsiteEntry } = useWebsiteController()
 
 onMounted(async () => {
 	await getWebsiteEntries()
 })
+
+const newWebsite = ref('')
+async function handleSubmit() {
+	await createWebsiteEntry({ url: newWebsite.value, blocked: true })
+	newWebsite.value = ''
+}
+
+const updateButtonDisabled = ref(true)
+watch(
+	() => state.value.websites,
+	(nv) => {
+		if (nv.length !== 0 && nv.length === initialWebsitesState.value.length) {
+			const currentStateChanged = nv.some((site, index) => {
+				if (site.blocked !== initialWebsitesState.value[index].blocked || site.url !== initialWebsitesState.value[index].url) {
+					return true
+				}
+
+				return false
+			})
+
+			updateButtonDisabled.value = !currentStateChanged
+		}
+	},
+	{
+		deep: true,
+	},
+)
+
+async function handleUpdate() {
+	await updateWebsiteEntries()
+	updateButtonDisabled.value = true
+}
 </script>
 
 <template>
-	<div>
-		<AppIconsSprite />
-		<main>
-			<RouterView />
-		</main>
-	</div>
+	<AppIconsSprite />
+	<article class="flex flex-col gap-y-4 p-4">
+		<form
+			class="flex gap-x-2"
+			@submit.prevent="handleSubmit"
+		>
+			<BaseInput
+				id="new-website"
+				v-model="newWebsite"
+				placeholder="youtube.com/shorts, x.com"
+			/>
+			<BaseButton
+				type="submit"
+				class="whitespace-nowrap"
+			>
+				Block Site
+			</BaseButton>
+		</form>
+		<ul
+			v-if="state.websites.length"
+			class="flex flex-col gap-y-2"
+		>
+			<li
+				v-for="(site, index) in state.websites"
+				:key="index"
+				class="flex items-center gap-x-4"
+			>
+				<BaseCheckbox
+					:id="`checkbox-${index}`"
+					v-model="site.blocked"
+				/>
+				<BaseInput
+					v-model="site.url"
+					placeholder="youtube.com/shorts or x.com"
+				/>
+
+				<BaseButton
+					variant="ghost"
+					icon="delete"
+					@click="deleteWebsiteEntry(index)"
+				/>
+			</li>
+		</ul>
+		<BaseButton
+			v-if="state.websites.length"
+			class="w-full"
+			:disabled="updateButtonDisabled"
+			@click="handleUpdate"
+		>
+			Sync changes
+		</BaseButton>
+	</article>
 </template>
